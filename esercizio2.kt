890a1464a8cfc8f53bd927a0dac33bd3317c32c9
Kay <124591761+moyomoyomoyo@users.noreply.github.com>
2025-11-13 20:26:43 +0100

    Implement basic math question app in Kotlin
    Create a simple Android app with a math question and buttons for answers.

---
+++ esercizio2.kt
@@ -0,0 +1,58 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                MyApp( modifier = Modifier.padding(innerPadding))
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    val question = "Quanto fa 2+2?"
+    var displayedText by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Text (text = question)
+        Button(onClick = { displayedText = "Risposta corretta!" }) {
+            Text("4")
+        }
+        Button(onClick = { displayedText = "Risposta errata!" }) {
+            Text("2")
+        }
+        Button(onClick = { displayedText = "Risposta errata!" }) {
+            Text("6")
+        }
+        Button(onClick = { displayedText = "Risposta errata!" }) {
+            Text("8")
+        }
+        Text(text = displayedText)
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun MyAppPreview() {
+    MyApp()
+}
