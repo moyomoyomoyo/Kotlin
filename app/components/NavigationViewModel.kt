5d9015674f9826d26167a1619da875754499fd22
Kay <124591761+moyomoyomoyo@users.noreply.github.com>
2025-11-20 23:00:52 +0100

    Update NavigationViewModel.kt

--- app/components/NavigationViewModel.kt
+++ app/components/NavigationViewModel.kt
@@ -1,15 +1,8 @@
-package com.example.myapp.components
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-
-class NavigationViewModel : ViewModel() {
     var currentScreen by mutableStateOf(Screen.HOME)
         private set
     var points = mutableStateOf(0)
     var next = mutableStateOf(0)
+    var totalQuestions = QuestionList().quiz.size
 
     fun navigateTo(screen: Screen) {
         currentScreen = screen
@@ -19,8 +12,13 @@ class NavigationViewModel : ViewModel() {
         points.value++
     }
 
+    fun returnPoints(): Int {
+        return points.value
+    }
+
     fun nextQuestion(){
         next.value++
     }
-
-}
+    fun nextQuestionIndex(): Int {
+        return next.value
+    }
