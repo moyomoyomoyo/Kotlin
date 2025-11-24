d1ba3b1afa64f04253f87884ac5339244fad14fd
Kay <124591761+moyomoyomoyo@users.noreply.github.com>
2025-11-24 10:42:38 +0100

    Create MainActivity.kt

---
+++ app2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapp.components.MainScreen
+import com.example.myapp.components.NavBar
+import com.example.myapp.components.NavigationViewModel
+import com.example.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    val nav: NavigationViewModel = viewModel()
+    Text(
+        text = "Hello!",
+        modifier = modifier
+    )
+    MainScreen(nav, modifier)
+    NavBar(nav, modifier)
+}
